ed26c55815a878f310fd13427a6cfac7cbf3c248
Trevor <trevorjbaron@gmail.com>
2014-03-18 00:27:13 -0400

    application download

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var path = require('path');
 var coyote = require('./lib/fileECoyote');
 
-coyote.startServer({port: 3008});
+coyote.startServer({
+										adaptor: new coyote.fileAdaptor(),
+										authKeys: ["app1SecretKey","app2SecretKey"],
+										downloadFolder: path.join(process.cwd(), "uploads"),
+										port: 3008
+									});
